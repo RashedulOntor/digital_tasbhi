6097989b11f660d5cd90d9544a611a92e02f411f
RashedulOntor <rashedulontor@gmail.com>
2026-08-31 00:32:24 +0600

    Initial commit

---
+++ app/src/main/java/com/example/digitaltasbhi/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.digitaltasbhi;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvCount;
+    Button buttonAdd, buttonSub, buttonRestart ;
+    int count =0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       tvCount = findViewById(R.id.tvCount);
+       buttonAdd =findViewById(R.id.buttonAdd);
+       buttonSub = findViewById(R.id.buttonSub);
+       buttonRestart = findViewById(R.id.buttonRestart);
+
+
+
+       buttonAdd.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+               count++ ;
+               tvCount.setText(""+count);
+           }
+
+       });
+
+
+        buttonSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (count > 0) {
+                    count--;
+                }
+
+                tvCount.setText(""+count);
+
+            }
+
+        });
+
+
+        buttonRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                count = 0 ;
+                tvCount.setText("0");
+
+            }
+
+        });
+
+
+
+
+    }
+
+
+}
